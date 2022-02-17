21b578e206438ea475767501e63327a97160e82f
EveSpiceapp <EveSpiceapp@gmail.com>
2022-02-17 16:19:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Student-Lesson-App
+New and updated student app which shows lessons. There are a lot of different new features added to the website
---
+++ lesson.js
@@ -0,0 +1,101 @@
+let lessons = [
+    {
+        "id": 0,
+        "Icon": "fa-atom",
+        "Subject": "Science",
+        "Location": "Hendon",
+        "Price": 185,
+        "Spaces": 5,
+        "Image": "./images/Science.png"
+    },
+    {
+        "id": 1,
+        "Icon": "fa-calculator",
+        "Subject": "Maths",
+        "Location": "Willesden",
+        "Price": 200,
+        "Spaces": 5,
+        "Image": "./images/maths-applications.jpg"
+
+    },
+    {
+        "id": 2,
+        "Icon": "fa-book",
+        "Subject": "English",
+        "Location": "Camden",
+        "Price": 120,
+        "Spaces": 5,
+        "Image": "./images/english gyal.jpg"
+
+    },
+    {
+        "id": 3,
+        "Icon": "fa-desktop",
+        "Subject": "ICT",
+        "Location": "Neasden",
+        "Price": 23,
+        "Spaces": 5,
+        "Image": "./images/Ict.jpg"
+
+    },
+    {
+        "id": 4,
+        "Icon": "fa-globe-europe",
+        "Subject": "Geography",
+        "Location": "Kilburn",
+        "Price": 90,
+        "Spaces": 5,
+        "Image": "./images/Geography.jpg"
+
+    },
+    {
+        "id": 5,
+        "Icon": "fa-landmark",
+        "Subject": "History",
+        "Location": "Harlesden",
+        "Price": 67,
+        "Spaces": 5,
+        "Image": "./images/History.jpg"
+
+    },
+    {
+        "id": 6,
+        "Icon": "fa-wrench",
+        "Subject": "Enineering",
+        "Location": "Acton",
+        "Price": 300,
+        "Spaces": 5,
+        "Image": "./images/Engineering.jpg"
+
+    },
+    {
+        "id": 7,
+        "Icon": "fa-paint-brush",
+        "Subject": "Art",
+        "Location": "Ladbroke Grove",
+        "Price": 100,
+        "Spaces": 5,
+        "Image": "./images/Art.jpg"
+
+    },
+    {
+        "id": 8,
+        "Icon": "fa-users",
+        "Subject": "Sociology",
+        "Location": "Brixton",
+        "Price": 250,
+        "Spaces": 5,
+        "Image": "./images/Sociology.jpg"
+
+    },
+    {
+        "id": 9,
+        "Icon": "fa-brain",
+        "Subject": "Phychology",
+        "Location": "Dollis Hill",
+        "Price": 150,
+        "Spaces": 5,
+        "Image": "./images/Phychology.jpg"
+
+    },
+]
